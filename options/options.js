355e1e65ace31b81969ef5c37295503620934ed2
Crowdbotics <team@crowdbotics.com>
2023-04-06 19:01:25 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "gratefulday-39758",
+  url: "https://gratefulday-39758.botics.co",
+  api: "https://gratefulday-39758.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
